e59ba1f041793d5f0390ae74d90b4f538823f800
Nashwan Sabti <nash.sabti@gmail.com>
2024-01-12 14:51:04 -0500

    working in N-D

--- test_factory/walker_ND.py
+++ test_factory/walker_ND.py
@@ -14,7 +14,7 @@ def initial_condition(a, b):
 def proposal_distribution(x, y):
     sigma = mx.array([1])
     return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(
-        -0.5 * mx.sum((y - x)**2) / sigma[0] ** 2
+        -0.5 * sum((y - x)**2) / sigma[0] ** 2
     )
 
 
@@ -25,7 +25,7 @@ def sample_proposal_distribution(current, key):
 
 def target_distributon(x):
     sigma = mx.array([1])
-    return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(-0.5 * mx.sum(x**2) / sigma[0] ** 2)
+    return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(-0.5 * sum(x**2) / sigma[0] ** 2)
 
 
 def acceptance_probability(current, proposal):
@@ -60,11 +60,9 @@ keys = mx.random.split(key, number_ini)
 
 result = mx.vmap(internal_function, in_axes=(0, 0))(x0_array, keys)
 result = np.array(result)
+print(result.shape)
 
-# plt.figure(figsize=(8, 5))
-# bins = mx.linspace(-5, 5, 30)
-# bin_centres = bins[:-1] + (bins[1:] - bins[:-1]) / 2
-# for i in range(number_ini):
-#     plt.hist(result[i], alpha=0.3, bins=bins, density=True)
-# plt.plot(np.array(bin_centres), np.array(target_distributon(bin_centres)))
-# plt.show()
+for numwalker in range(number_ini):
+    plt.scatter(result[:,numwalker,0], result[:,numwalker,1], s=4)
+
+plt.show()

17fd2aa6d9ce8b0f8182af4766b6d830b54e127d
Nashwan Sabti <nash.sabti@gmail.com>
2024-01-12 15:21:28 -0500

    make samplex class

--- test_factory/walker_ND.py
+++ test_factory/walker_ND.py
@@ -6,63 +6,66 @@ from tqdm import tqdm
 
 mx.random.seed(np.random.randint(0, 1000))
 
+class samplex():
+    def __init__(self, Nwalkers, Ndim):
 
-def initial_condition(a, b):
-    return mx.random.uniform(a, b, shape=a.shape)
+        self.x0_array = self.initial_condition(mx.zeros((Nwalkers, Ndim)) - 5, 5.0)
+        self.key = mx.random.key(1234)
+        self.keys = mx.random.split(self.key, Nwalkers)
+
+    def initial_condition(self, a, b):
+        return mx.random.uniform(a, b, shape=a.shape)
+
+    def proposal_distribution(self, x, y):
+        sigma = mx.array([1])
+        return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(
+            -0.5 * sum((y - x)**2) / sigma[0] ** 2
+        )
+
+    def sample_proposal_distribution(self, current, key):
+        sigma = 1
+        return current + sigma * mx.random.normal(key=key, shape=current.shape)
+
+    def target_distributon(self, x):
+        sigma = mx.array([1])
+        return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(-0.5 * sum(x**2) / sigma[0] ** 2)
+
+    def acceptance_probability(self, current, proposal):
+        prob = (
+            self.target_distributon(proposal)
+            * self.proposal_distribution(current, proposal)
+            / (self.target_distributon(current) * self.proposal_distribution(proposal, current))
+        )
+        return mx.minimum(1.0, prob)
+
+    def internal_function(self, x0, key, steps):
+        xcurrent = x0
+        states = []
+        step_key = mx.random.split(key, len(steps))
+        step_key2 = mx.random.split(step_key[0], len(steps))
+        for step in tqdm(steps):
+            states.append(xcurrent)
+            xproposal = self.sample_proposal_distribution(xcurrent, step_key[step])
+            prob = self.acceptance_probability(xcurrent, xproposal)
+            rand = mx.random.uniform(key=step_key2[step])
+            xcurrent = mx.where(prob > rand, xproposal, xcurrent)
+        return states
+
+    def run(self, Nsteps):
+        steps = mx.arange(Nsteps)
+        result = mx.vmap(self.internal_function, in_axes=(0, 0, None))(self.x0_array, self.keys, steps)
+        return np.array(result)
 
 
-def proposal_distribution(x, y):
-    sigma = mx.array([1])
-    return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(
-        -0.5 * sum((y - x)**2) / sigma[0] ** 2
-    )
+if __name__ == "__main__":
+    Nwalkers = 5
+    Ndim = 2
+    Nsteps = 3000
 
+    sam = samplex(Nwalkers, Ndim)
+    result = sam.run(Nsteps)
 
-def sample_proposal_distribution(current, key):
-    sigma = 1
-    return current + sigma * mx.random.normal(key=key, shape=current.shape)
+    for numwalker in range(Nwalkers):
+        plt.scatter(result[:,numwalker,0], result[:,numwalker,1], s=4)
 
-
-def target_distributon(x):
-    sigma = mx.array([1])
-    return (1 / mx.sqrt(2 * pi * sigma**2))[0] * mx.exp(-0.5 * sum(x**2) / sigma[0] ** 2)
-
-
-def acceptance_probability(current, proposal):
-    prob = (
-        target_distributon(proposal)
-        * proposal_distribution(current, proposal)
-        / (target_distributon(current) * proposal_distribution(proposal, current))
-    )
-    return mx.minimum(1.0, prob)
-
-
-def internal_function(x0, key):
-    steps = mx.arange(3000)
-    xcurrent = x0
-    states = []
-    step_key = mx.random.split(key, len(steps))
-    step_key2 = mx.random.split(step_key[0], len(steps))
-    for step in tqdm(steps):
-        states.append(xcurrent)
-        xproposal = sample_proposal_distribution(xcurrent, step_key[step])
-        prob = acceptance_probability(xcurrent, xproposal)
-        rand = mx.random.uniform(key=step_key2[step])
-        xcurrent = mx.where(prob > rand, xproposal, xcurrent)
-    return states
-
-
-number_ini = 5
-ndim = 2
-x0_array = initial_condition(mx.zeros((number_ini, ndim)) - 5, 5.0)
-key = mx.random.key(1234)
-keys = mx.random.split(key, number_ini)
-
-result = mx.vmap(internal_function, in_axes=(0, 0))(x0_array, keys)
-result = np.array(result)
-print(result.shape)
-
-for numwalker in range(number_ini):
-    plt.scatter(result[:,numwalker,0], result[:,numwalker,1], s=4)
-
-plt.show()
+    plt.show()

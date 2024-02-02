200fb4440b6064c950a1995ba4e2fb3039a6d58b
Nashwan Sabti <nash.sabti@gmail.com>
2024-02-02 16:11:18 -0500

    format example

--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -13,44 +13,25 @@ def initial_condition(a, b):
     return mx.random.uniform(a, b, shape=a.shape)
 
 
-def generate_data():
-    # Parameters we are trying to infer
-    m_true = 2.0
-    c_true = 3.0
-    b_true = 1.0
+def log_target_distribution(theta, data):
+    m, c, b = theta
+    x, y, sigma = data
+    model = b * x**2 + m * x + c
+    residual = y - model
+    return sum(-0.5 * (residual**2 / sigma**2))
 
+
+def generate_data(b_true, m_true, c_true):
     x = mx.linspace(-5, 5, 20)
-    err = mx.random.normal(x.shape)  # * 10
+    err = mx.random.normal(x.shape)
     y = b_true * x**2 + m_true * x + c_true + err
+    return x, y, err
 
-    def log_target_distribution(theta, data):
-        m, c, b = theta
-        x, y, sigma = data
-        model = b * x**2 + m * x + c
-        residual = y - model
-        return sum(
-            -0.5 * (residual**2 / sigma**2)
-        )  # + mx.log(mx.sqrt(2 * pi * sigma**2)))
 
-    logtarget = lambda theta: log_target_distribution(theta, (x, y, err))
+def make_plots(x, y, err, sam, b_true, m_true, c_true):
+    # Plot cornerplot and bestfit
 
-    Nwalkers = 32
-    Ndim = 3
-    Nsteps = 10_000
-    cov_matrix = mx.array([0.01, 0.01, 0.01])
-    jumping_factor = 1.0
-
-    x0_array = initial_condition(mx.zeros((Nwalkers, Ndim)) + 1, 5.0)
-
-    sampler = MH_Gaussian_sampler(logtarget)
-    sam = samplex(sampler, Nwalkers)
-    result = sam.run(Nsteps, x0_array, cov_matrix, jumping_factor)
-    # Should be (Nsteps, Nwalkers, Ndim)
-    print(result.shape)
     burned_inchains = np.array(sam.get_chains(remove_burnin=True))
-    print(burned_inchains.shape)
-    # quit()
-    # result_stacked = np.array(sam.get_chains(discard=1000, flat=True))
 
     names = [
         r"$m$",
@@ -68,31 +49,10 @@ def generate_data():
         names=names,
         num_bins=[50, 50, 20],
         lims=lims,
-        # interpolation_smoothing=2.0,
-        # gaussian_smoothing=0.01
-        # fig=None,
-        # axes=None,
     )
     plt.show()
 
-    # alpha_range = np.linspace(0.1, 1, Nsteps)
-
-    # for numwalker in range(Nwalkers):
-    #     plt.scatter(
-    #         result[:, numwalker, 1], result[:, numwalker, 2], s=10, alpha=alpha_range
-    #     )
-
-    # plt.show()
-
     plt.figure(figsize=(10, 5))
-    # plt.errorbar(
-    #     x.tolist(),
-    #     y.tolist(),
-    #     yerr=mx.abs(err).tolist(),
-    #     fmt=".k",
-    #     capsize=0,
-    #     alpha=0.5,
-    # )
     plt.errorbar(
         x.tolist(),
         y.tolist(),
@@ -126,4 +86,23 @@ def generate_data():
 
 
 if __name__ == "__main__":
-    generate_data()
+    # Parameters we are trying to infer
+    b_true = 1.0
+    m_true = 2.0
+    c_true = 3.0
+    x, y, err = generate_data(b_true, m_true, c_true)
+
+    logtarget = lambda theta: log_target_distribution(theta, (x, y, err))
+
+    Nwalkers = 32
+    Ndim = 3
+    Nsteps = 10_000
+    cov_matrix = mx.array([0.01, 0.01, 0.01])
+    jumping_factor = 1.0
+
+    x0_array = initial_condition(mx.zeros((Nwalkers, Ndim)) + 1, 5.0)
+
+    sampler = MH_Gaussian_sampler(logtarget)
+    sam = samplex(sampler, Nwalkers)
+    sam.run(Nsteps, x0_array, cov_matrix, jumping_factor)
+    make_plots(x, y, err, sam, b_true, m_true, c_true)

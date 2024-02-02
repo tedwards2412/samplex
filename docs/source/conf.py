de2d714863005894a07994e2fc48f9d8b8e64ff5
tedwards2412 <tedwards2412@gmail.com>
2024-02-02 13:51:32 -0500

    trying to fix readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -*- coding: utf-8 -*-
 
